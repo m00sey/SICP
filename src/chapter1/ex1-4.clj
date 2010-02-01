e6820189217ec4a4374a6ff02ca6966e565c987a
Kevin Griffin <griffin.kev@gmail.com>
2010-01-31 19:32:19 -0500

    added comment

--- src/chapter1/ex1-4.clj
+++ src/chapter1/ex1-4.clj
@@ -1,3 +1,4 @@
+;;Exercise 1.4.
 ;Observe that our model of evaluation allows for combinations whose operators are compound expressions.
 ;Use this observation to describe the behavior of the following procedure:
 
